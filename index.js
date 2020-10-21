8133f49728892bc5962581716ae95ae412ae8a6e
JsMaster <callitmyemail@protonmail.com>
2020-10-21 17:34:14 +0300

    Too many changes to say : )

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 'use strict'
 
+const path = require('path')
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose')
 
 const registerRouter = require('./routes/signup')
 const loginRouter = require('./routes/signin')
 
+const viewsPath = path.join(__dirname, '/templates/views')
+const partialsDirectory = path.join(__dirname, '/templates/partials')
+
+require('hbs').registerPartials(partialsDirectory)
+
 app.set('view engine', 'hbs')  // setting up the view engine
+app.set('views', viewsPath)
+
+
 
 app.use(express.static('assets'))  //setting up static assets
 
